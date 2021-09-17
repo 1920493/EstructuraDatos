7b69afe33576391524335d322e633cc5294a8111
1920493 <1920493@cesba-queretaro.edu.mx>
2021-09-16 20:26:00 -0500

    Estructura_Datos1

---
+++ src/java/dto/ED_Domicilio.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dto;
+
+/**
+ *
+ * @author Artefacto
+ */
+/**
+ *
+ * @author Artefacto
+ */
+public class ED_Domicilio {
+    public int id;
+    public String calle;
+    public String numero;
+    public String colonia;
+    public int cp;
+    public String municipio;
+    public String estatus;
+    public int idPersona;
+}
---
+++ src/java/dto/ED_Persona.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dto;
+
+
+/**
+ *
+ * @author Artefacto
+ */
+public class ED_Persona {
+    public int id;
+    public String nombre;
+    public String apellidoPaterno;
+    public String apellidoMaterno;
+    public String fechaNacimiento;
+    public String estatus;
+    
+}
